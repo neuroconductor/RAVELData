986c699550d7630d4a17399fbbd82a7052555bcc
Jean-Philippe Fortin <fortin.jeanphilippe@gene.com>
2020-08-21 16:55:44 -0700

    Cleaned code a little

--- inst/preprocess.R
+++ inst/preprocess.R
@@ -39,26 +39,22 @@ segs  <- pblapply(scans_n4_reg_brain, function(x){
 	fast(file=x, outfile="try", opts= "-t 1 -n 3 --nobias", retimg=TRUE, verbose=FALSE)	
 })
 system("rm try*")
-
 segs_csf <- pblapply(segs, function(x){
 	x[x!=1] <- 0
 	x
 })
-
 # Writing out the processed images:
-pblapply(1:4, function(j){antsImageWrite(scans[[j]], paste0("extdata/scan",j,".nii.gz"))})
-pblapply(1:4, function(j){antsImageWrite(scans_n4_reg_brain[[j]], paste0("extdata/scan",j,"_processed.nii.gz"))})
-pblapply(1:4, function(j){writeNIfTI(segs[[j]], paste0("extdata/scan",j,"_seg.nii.gz"))})
-pblapply(1:4, function(j){writeNIfTI(segs_csf[[j]], paste0("extdata/scan",j,"_csf_mask.nii.gz"))})
-
+pblapply(1:4, function(j){
+	prefix <- "extdata/scan"
+	antsImageWrite(scans[[j]], paste0(prefix, j, ".nii.gz"))
+	antsImageWrite(scans_n4_reg_brain[[j]], paste0(prefix, j, "_processed.nii.gz"))
+	writeNIfTI(segs[[j]], paste0(prefix, j, "_seg"))
+	writeNIfTI(segs_csf[[j]], paste0(prefix, j, "_csf_mask"))
+})
 
 #Let's create the mask intersect:
-#dir <- file.path(find.package("RAVELData"), "extdata")
-dir   <- "./extdata"
-masks <- list.files(dir, full.names=TRUE, pattern="*mask*.nii*")
-output.file <- "extdata/csf_mask_intersection.nii.gz"
-mask <- maskIntersect(masks, output.file=output.file)
-
+masks <- list.files("extdata", full.names=TRUE, pattern="*mask*.nii*")
+mask  <- maskIntersect(masks, output.file="extdata/csf_mask_intersection.nii.gz")
 
 
 # Alternative segmentation with atropos from ANTs:
@@ -68,33 +64,18 @@ mask <- maskIntersect(masks, output.file=output.file)
 #  x[x!=1] <- 0
 #  x
 #})
-#pblapply(1:4, function(j){writeNIfTI(segs[[j]], paste0("extdata/scan",j,"_seg.nii.gz"))})
-#pblapply(1:4, function(j){writeNIfTI(segs_csf[[j]], paste0("extdata/scan",j,"_csf_mask.nii.gz"))})
-
+#pblapply(1:4, function(j){writeNIfTI(segs[[j]], paste0("extdata/scan",j,"_seg"))})
+#pblapply(1:4, function(j){writeNIfTI(segs_csf[[j]], paste0("extdata/scan",j,"_csf_mask"))})
 
 
 #pdf("try.pdf", width=5, height=5)
 #multi_overlay(scans_n4_reg_brain)
 #dev.off()
-
-
-# files <- list.files("../data/", pattern="processed.nii.gz", full.names=TRUE)
-# scans_n4_reg_brain <- lapply(files, readNIfTI)
-
-
-
 # multi_overlay(scans_n4_reg_brain, 
 #   par.opts=list(mfrow=c(2,2),bg = "black",oma = c(0, 0, 0, 0), mar = rep(0, 4)))
 # dev.off()
 
-
-
-
-
-
-
-
-
-
+# files <- list.files("../data/", pattern="processed.nii.gz", full.names=TRUE)
+# scans_n4_reg_brain <- lapply(files, readNIfTI)
 
 
