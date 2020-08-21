2ab8d29d84b02b88de6f56666f6811d38e3194f1
Jean-Philippe Fortin <fortin.jeanphilippe@gene.com>
2020-08-21 16:52:26 -0700

    Updated preprocessing script

--- inst/preprocess.R
+++ inst/preprocess.R
@@ -4,35 +4,35 @@ library(ANTsR)
 library(extrantsr)
 library(fslr)
 library(RAVEL)
+library(EveTemplate)
 
+# Getting EveTemplate data first:
+template   <- antsImageRead(EveTemplate::getEvePath(),3) # Eve template:
+brain_mask <- antsImageRead(EveTemplate::getEvePath("Brain_Mask"), 3)
 
+# Getting KIRBY21 data:
 kirbyDir  <- system.file(package = "kirby21.scan.1")
-files     <- list.files(kirbyDir, recursive=TRUE, pattern="MPRAGE.nii.gz", full.names=TRUE)[1:4]
+files <- list.files(kirbyDir,
+	recursive=TRUE,
+	pattern="MPRAGE.nii.gz",
+	full.names=TRUE
+)[1:4]
 
 # Reading the files:
 scans <- pblapply(files, function(x) antsImageRead(x, 3))
 # N4 Correction:
 scans_n4 <- pblapply(scans, n4BiasFieldCorrection)
 # Non-linear registration:
-template  <- antsImageRead("../data/JHU_MNI_SS_T1.nii.gz",3) # Eve template:
 scans_n4_reg <- pblapply(scans_n4, function(x){
 	warp <- antsRegistration(fixed = template, moving = x, typeofTransform = "SyN")
 	temp <- antsImageClone(warp$warpedmovout)
 	temp
 })
 # Brain extraction:
-brain_mask <- antsImageRead("../data/JHU_MNI_SS_T1_Brain_Mask.nii.gz", 3)
 scans_n4_reg_brain <- pblapply(scans_n4_reg, function(x){
 	x[brain_mask==0] <- 0
 	x
 })
-
-# Writing out the processed images:
-pblapply(1:4, function(j){antsImageWrite(scans[[j]], paste0("../data/scan",j,"_raw.nii.gz"))})
-pblapply(1:4, function(j){antsImageWrite(scans_n4_reg[[j]], paste0("../data/scan",j,"_n4_registered.nii.gz"))})
-pblapply(1:4, function(j){antsImageWrite(scans_n4_reg_brain[[j]], paste0("../data/scan",j,"_processed.nii.gz"))})
-
-
 # Segmentation with FSL FAST:
 scans_n4_reg_brain <- pblapply(scans_n4_reg_brain, ants2oro)
 segs  <- pblapply(scans_n4_reg_brain, function(x){
@@ -45,32 +45,31 @@ segs_csf <- pblapply(segs, function(x){
 	x
 })
 
-pblapply(1:4, function(j){writeNIfTI(segs[[j]], paste0("../inst/extdata/scan",j,"_seg.nii.gz"))})
-pblapply(1:4, function(j){writeNIfTI(segs_csf[[j]], paste0("../inst/extdata/scan",j,"_csf_mask.nii.gz"))})
+# Writing out the processed images:
+pblapply(1:4, function(j){antsImageWrite(scans[[j]], paste0("extdata/scan",j,".nii.gz"))})
+pblapply(1:4, function(j){antsImageWrite(scans_n4_reg_brain[[j]], paste0("extdata/scan",j,"_processed.nii.gz"))})
+pblapply(1:4, function(j){writeNIfTI(segs[[j]], paste0("extdata/scan",j,"_seg.nii.gz"))})
+pblapply(1:4, function(j){writeNIfTI(segs_csf[[j]], paste0("extdata/scan",j,"_csf_mask.nii.gz"))})
+
+
+#Let's create the mask intersect:
+#dir <- file.path(find.package("RAVELData"), "extdata")
+dir   <- "./extdata"
+masks <- list.files(dir, full.names=TRUE, pattern="*mask*.nii*")
+output.file <- "extdata/csf_mask_intersection.nii.gz"
+mask <- maskIntersect(masks, output.file=output.file)
 
 
 
 # Alternative segmentation with atropos from ANTs:
-scans_n4_reg_brain <- pblapply(scans_n4_reg_brain, oro2ants)
-segs  <- pblapply(scans_n4_reg_brain, otropos)
-
-
-
-
-segs_csf <- pblapply(segs, function(x){
-  x[x!=1] <- 0
-  x
-})
-
-pblapply(1:4, function(j){writeNIfTI(segs[[j]], paste0("../inst/extdata/scan",j,"_seg.nii.gz"))})
-pblapply(1:4, function(j){writeNIfTI(segs_csf[[j]], paste0("../inst/extdata/scan",j,"_csf_mask.nii.gz"))})
-
-
-#Let's create the mask intersect:
-dir <- file.path(find.package("RAVELData"), "extdata")
-masks <- list.files(dir, full.names=TRUE, pattern="*mask*.nii*")
-output.file <- "extdata/csf_mask_intersection.nii.gz"
-mask <- maskIntersect(masks, output.file=output.file)
+#scans_n4_reg_brain <- pblapply(scans_n4_reg_brain, oro2ants)
+#segs  <- pblapply(scans_n4_reg_brain, otropos)
+#segs_csf <- pblapply(segs, function(x){
+#  x[x!=1] <- 0
+#  x
+#})
+#pblapply(1:4, function(j){writeNIfTI(segs[[j]], paste0("extdata/scan",j,"_seg.nii.gz"))})
+#pblapply(1:4, function(j){writeNIfTI(segs_csf[[j]], paste0("extdata/scan",j,"_csf_mask.nii.gz"))})
 
 
 
